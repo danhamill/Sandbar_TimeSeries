de9110088253adb39791e93236055aaa4c8523a6
danhamill <hamill.daniel@gmail.com>
2016-05-03 12:46:25 -0700

    Finished the lookups

--- Scripts/sandbar_datbase_complier.py
+++ Scripts/sandbar_datbase_complier.py
@@ -11,8 +11,8 @@ import numpy as np
 max_vol = r'C:\workspace\Sandbar_Process\LU_Max_Vol.xlsx'
 bar_compile = r'C:\workspace\Sandbar_Process\csv_output\Sandbar_data.csv'
 bar_trip = r'C:\workspace\Sandbar_Process\LU_Site_location_time_Series.csv' 
-outFileName = r'C:\workspace\Sandbar_Process\Merged_Sandbar_data.csv'
 trip_dates = r'C:\workspace\Sandbar_Process\Date_Error_lookup.xlsx'
+outFileName = r'C:\workspace\Sandbar_Process\Merged_Sandbar_data.csv'
 
 #Load in bar compile
 data = pd.read_csv(bar_compile, sep=',')
@@ -32,7 +32,7 @@ data4 = data[data.SitePart=='Channel']
 data4 = data4.assign(Plane_Height='chanminto8k')
 frames = [data1,data2,data3,data4]
 data = pd.concat(frames)
-data = data.set_index(['Site','SurveyDate','Plane_Height'])
+del frames
 del data1, data2, data3, data4
 
 
@@ -42,36 +42,35 @@ lu_3['SurveyDate']= pd.to_datetime(lu_3['SurveyDate'], format='%m/%d/%Y')
 lu_3['TripDate']= pd.to_datetime(lu_3['TripDate'], format='%m/%d/%Y')
 lu_3 = lu_3[['Site','SurveyDate','Bin','TripDate']]
 lu_3 = lu_3.rename(columns={'Bin':'Plane_Height'})
-lu_3 = lu_3.set_index(['Site','SurveyDate','Plane_Height'])
 
+#Drop Duplicates
+lu_3 = lu_3.groupby(['Site','SurveyDate','Plane_Height']).first().reset_index()
+lu_3['Site']=lu_3['Site'].str.lower()
+lu_3 = lu_3.reset_index()
+data = data.merge(lu_3,left_on = ['Site','SurveyDate','Plane_Height'],right_on = ['Site','SurveyDate','Plane_Height'],how='left')
+data = data.drop(data.columns[[-2]],axis=1)
 
+#Add Dates for sep and reatt bars
+data1 = data[data.TripDate.isnull()]
+data1 = data1.reset_index()
+data1['tmpsite'] = data1['Site'].str[:4]
+data1 = data1.merge(lu_3,left_on = ['tmpsite','SurveyDate','Plane_Height'],right_on = ['Site','SurveyDate','Plane_Height'],how='left')
+data1 = data1.drop(data1.columns[-5:-1],axis=1)
+data1 = data1.drop(data1.columns[0],axis=1)
+data1 = data1.rename(columns = {'TripDate_y': 'TripDate'})
+data1 = data1.rename(columns = {'Site_x': 'Site'})
+data2 = data[data.TripDate.notnull()]
+data = pd.concat([data1,data2])
 
-##############Need to sort out the trip date lookup duplicates!!!!!!!!!!!!!!!!!!!!!
-tmp = data.update(lu_3,join='left')
-
-#tmp = data.reset_index().merge(lu_3.reset_index(), on=['Site','SurveyDate','Plane_Height'], how = 'left')
-#tmp1 = data.merge(lu_3, how='left', left_on=['Site','SurveyDate','Plane_Height'], right_on=['Site','SurveyDate','Plane_Height'])
-
-tmp = data.merge(lu_3,left_index=True,right_index=True,how='left')
-tmp = tmp.drop(tmp.columns[[2]], axis=1)
-tmp = tmp.rename(columns={'Site_x':'Site'})
-
-tmp = tmp_data.merge(lu_3,on=['Site','SurveyDate'], how='left').set_index('Site')
-tmp = data.merge(lu_3,left_index=True,right_index=True,how='left', on=['SurveyDate'])
-tmp = data.merge(lu_3, left_on='SurveyDate', right_on='SurveyDate',left_index=True,right_index=True)
+del lu_3, data1, data2
 
 #Merge bar_compile with bar_trip
-lu_1 = pd.read_csv(bar_trip, sep=',',index_col=[0])
+data = data.set_index(['Site'])
+lu_1 = pd.read_csv(bar_trip, sep='\t',index_col=[0])
 data = pd.merge(data,lu_1,left_index=True,right_index=True,how='left')
 
 #Load max_vol
 lu_2 = pd.read_excel(max_vol, index_col=[0])
-
-
-
-
-
-
 
 #Format max_vol for merging
 chan_MaxVol = lu_2[['MaxVol_ChMinTo8k']].dropna(axis=0)
@@ -82,7 +81,10 @@ eddy_fz_MaxVol = lu_2[['MaxVol_Ed8kto25k']].dropna(axis=0)
 eddy_fz_MaxVol = eddy_fz_MaxVol.rename(columns = {'MaxVol_Ed8kto25k':'MaxVol'})
 eddy_he_MaxVol = lu_2[['MaxVol_EdAbv25k']].dropna(axis=0)
 eddy_he_MaxVol = eddy_he_MaxVol.rename(columns = {'MaxVol_EdAbv25k':'MaxVol'})
-
+eddy_max_area = lu_2[['Max_Area_Eddy']].dropna(axis=0)
+eddy_max_area = eddy_max_area.rename(columns={'Max_Area_Eddy':'Max_Area'})
+chan_max_area = lu_2[['Max_Area_Channel']].dropna(axis=0)
+chan_max_area = chan_max_area.rename(columns={'Max_Area_Channel':'Max_Area'})
 del lu_1, lu_2
 
 #Append maxiumum volumes in subsets
@@ -99,7 +101,18 @@ del chan_MaxVol, eddy_fz_MaxVol, eddy_he_MaxVol, eddy_low_MaxVol
 frames = [data1,data2,data3,data4]
 data = pd.concat(frames)
 
-del data1, data2, data3, data4
+del data1, data2, data3, data4, frames
 #Get rid of total eddy records
+
 data = data[np.isfinite(data['MaxVol'])]
+
+#Merge max areas
+data1= data[(data.SitePart == 'Eddy')] 
+data1 = data1.merge(eddy_max_area,left_index=True,right_index=True,how='left')
+data2= data[(data.SitePart == 'Channel')] 
+data2 = data2.merge(chan_max_area,left_index=True,right_index=True,how='left')
+data = pd.concat([data1,data2])
+
+del data1,data2,chan_max_area,eddy_max_area
+
 data.to_csv(outFileName)
