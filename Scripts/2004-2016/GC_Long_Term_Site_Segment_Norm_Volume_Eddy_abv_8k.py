4ae8edf6f39f25c0e3b4ba04b186f97dc75d66f6
danhamill <hamill.daniel@gmail.com>
2016-09-26 10:13:48 -0700

    Added error bars

--- Scripts/2004-2016/GC_Long_Term_Site_Segment_Norm_Volume_Eddy_abv_8k.py
+++ Scripts/2004-2016/GC_Long_Term_Site_Segment_Norm_Volume_Eddy_abv_8k.py
@@ -25,10 +25,22 @@ query1 = data[(data.Segment != '1_UMC') & (data.Segment != '2_LMC') & (data.Segm
 (data.SitePart == 'Eddy') & (data.Plane_Height != 'eddyminto8k') & (data.SiteRange=='long')]
 query1 = query1[query1['TripDate'] > '2004-01-01']
 
+#Determine errors for norm volume
+temp = query1
+temp['NormVol'] = temp['Volume']/temp['MaxVol']
+tmp_pvt = pd.pivot_table(temp, values=['NormVol'], index=['TripDate'], aggfunc=np.std)
+tmp_pvt = tmp_pvt.rename(columns={'NormVol':'std_dev'})
+tmp_count = pd.pivot_table(temp,values=['NormVol'], index=['TripDate'], aggfunc='count')
+tmp_count = tmp_count.rename(columns={'NormVol':'count'})
+tmp_pvt['std_error'] = tmp_pvt['std_dev']/np.sqrt(tmp_count['count'])
+tmp_pvt = tmp_pvt[['std_error']]
+
 #Determine what the earliest survey was for each site
 table1 = pd.pivot_table(query1, values=['Volume', 'MaxVol'], index=['TripDate'], aggfunc=np.sum)
 table1['NormVol']= table1['Volume']/table1['MaxVol']
 table1 = table1[['NormVol']]
+table1 = table1.merge(tmp_pvt, left_index=True, right_index=True, how='left')
+del temp, tmp_pvt, tmp_count
 
 
 
@@ -37,24 +49,46 @@ query2 = data[(data.Segment != '1_UMC') & (data.Segment != '2_LMC') & (data.Segm
 (data.SitePart == 'Eddy') & (data.Plane_Height != 'eddyminto8k') & (data.SiteRange=='long')]
 query2 = query2[query2['TripDate'] > '2004-01-01']
 
+temp = query2
+temp['NormVol'] = temp['Volume']/temp['MaxVol']
+tmp_pvt = pd.pivot_table(temp, values=['NormVol'], index=['TripDate'], aggfunc=np.std)
+tmp_pvt = tmp_pvt.rename(columns={'NormVol':'std_dev'})
+tmp_count = pd.pivot_table(temp,values=['NormVol'], index=['TripDate'], aggfunc='count')
+tmp_count = tmp_count.rename(columns={'NormVol':'count'})
+tmp_pvt['std_error'] = tmp_pvt['std_dev']/np.sqrt(tmp_count['count'])
+tmp_pvt = tmp_pvt[['std_error']]
+
 table2 = pd.pivot_table(query2, values=['Volume', 'MaxVol'], index=['TripDate'], aggfunc=np.sum)
 table2['NormVol']= table2['Volume']/table2['MaxVol']
 table2 = table2[['NormVol']]
+table2 = table2.merge(tmp_pvt, left_index=True, right_index=True, how='left')
+del temp, tmp_pvt, tmp_count
 
 #Western Grand Canyon
 query3 = data[(data.Segment != '1_UMC') & (data.Segment != '2_LMC') & (data.Segment != '3_EGC') & (data.Segment != '4_CGC') & 
 (data.SitePart == 'Eddy') & (data.Plane_Height != 'eddyminto8k') & (data.SiteRange=='long')]
 query3 = query3[query3['TripDate'] > '2004-01-01']
 
+temp = query3
+temp['NormVol'] = temp['Volume']/temp['MaxVol']
+tmp_pvt = pd.pivot_table(temp, values=['NormVol'], index=['TripDate'], aggfunc=np.std)
+tmp_pvt = tmp_pvt.rename(columns={'NormVol':'std_dev'})
+tmp_count = pd.pivot_table(temp,values=['NormVol'], index=['TripDate'], aggfunc='count')
+tmp_count = tmp_count.rename(columns={'NormVol':'count'})
+tmp_pvt['std_error'] = tmp_pvt['std_dev']/np.sqrt(tmp_count['count'])
+tmp_pvt = tmp_pvt[['std_error']]
+
 table3 = pd.pivot_table(query3, values=['Volume', 'MaxVol'], index=['TripDate'], aggfunc=np.sum)
 table3['NormVol']= table3['Volume']/table3['MaxVol']
 table3 = table3[['NormVol']]
+table3 = table3.merge(tmp_pvt, left_index=True, right_index=True, how='left')
+del temp, tmp_pvt, tmp_count
 
-with PdfPages(r'C:\workspace\Time_Series\Output\GC_long_Term_Norm_Vol_eddyabv8k.pdf') as pdf:
+with PdfPages(r'C:\workspace\Time_Series\Output\2004-2015\GC_long_Term_Norm_Vol_eddyabv8k_w_err_bars.pdf') as pdf:
     fig, ax = plt.subplots(figsize=(7,6),nrows=1)
-    table1.plot(y = 'NormVol', ax = ax, label = 'Eastern Grand Canyon')
-    table2.plot(y = 'NormVol', ax = ax, label = 'Central Grand Canyon')
-    table3.plot(y = 'NormVol', ax = ax, label = 'Western Grand Canyon')
+    table1.plot(y = 'NormVol', yerr = 'std_error', ax = ax, label = 'Eastern Grand Canyon')
+    table2.plot(y = 'NormVol', yerr = 'std_error', ax = ax, label = 'Central Grand Canyon')
+    table3.plot(y = 'NormVol', yerr = 'std_error', ax = ax, label = 'Western Grand Canyon')
     ax.set_ylabel('Normalized Volume [m$^3$/m$^3$] \n Normalized to Maximum Volume')
     ax.set_title('Long Term Monitoring Sites: Eddy above 8k')   
     pdf.savefig()
