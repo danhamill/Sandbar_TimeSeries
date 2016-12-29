e14a64f39cd21647ca33bb3bb4a545516716c76d
danhamill <hamill.daniel@gmail.com>
2016-12-29 13:55:05 -0700

    Inital go around with influnce df, should change to be percents

--- Scripts/mc_variabilit.py
+++ Scripts/mc_variabilit.py
@@ -10,16 +10,19 @@ import pandas as pd
 import platform
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 def query_df(df):
     '''
     Functon to subset data dataframe to Marble Canyon Sediment Enricnment Period
     '''
+    tmp = df
     mc_query = 'Segment == ["1_UMC","2_LMC"]'
-    df.query(mc_query,inplace=True)
-    df.query('Period == ["Sediment_Enrichment"]',inplace = True)
-    df.query('Time_Series ==["long"]')
-    df['TripDate'] = pd.to_datetime(df['TripDate'], format='%Y-%m-%d')
-    return df
+    tmp.query(mc_query,inplace=True)
+    tmp.query('Period == ["Sediment_Enrichment"]',inplace = True)
+    tmp.query('Time_Series ==["long"]')
+    tmp['TripDate'] = pd.to_datetime(tmp['TripDate'], format='%Y-%m-%d')
+    return tmp
 
     
 def pivot_df(df):
@@ -29,14 +32,54 @@ def pivot_df(df):
     lt_pvt = lt_pvt[['Norm_Vol','Norm_Error']]
     return lt_pvt
     
+
+
+def change_finder(extra_sites,long_term, all_sites):
+    
+    for n in xrange(len(extra_sites)):
+        
+        #subset all sites to drop one value
+        drop_site = extra_sites.iloc[n][0]
+        sites2test =all_sites[all_sites.Site != drop_site]
+    
+        #Find normal Volumes 
+        long_t = pivot_df(long_term)
+        all_s = pivot_df(sites2test)
+    
+        long_t= pd.pivot_table(long_t.reset_index(), index=['TripDate'], values=['Norm_Vol'])
+        all_s = pd.pivot_table(all_s.reset_index(), index=['TripDate'], values=['Norm_Vol'])
+        
+        if n == 0:
+            print n
+            in_df=None
+            df_back = merge_df(long_t,all_s,drop_site,in_df)
+        else:
+            print n
+            df_back = merge_df(long_t,all_s,drop_site,df_back)
+        
+    return df_back
+        
+def merge_df(long_t,all_s, drop_site,in_df):
+    if in_df is None:
+        print 'Thinks in_df is None...'
+        vol_diff = long_t['Norm_Vol'].sum(axis=0)-all_s['Norm_Vol'].sum(axis=0)
+        out_df = pd.DataFrame(data=[vol_diff], columns=['Vol_Change'],index=[drop_site])
+        return out_df
+    elif in_df is not None:
+        print 'Found in_df...'
+        vol_diff = long_t['Norm_Vol'].sum(axis=0)-all_s['Norm_Vol'].sum(axis=0)
+        out_df = pd.concat([in_df,pd.DataFrame(data=[vol_diff], columns=['Vol_Change'],index=[drop_site])])
+        return out_df
+
 if platform.system() == 'Darwin':
     db_file = '/Users/danielhamill/git_clones/sandbar_process/Merged_Sandbar_data.csv'
     lt_sites ='/Users/danielhamill/git_clones/Time_Series/sites.xlsx'
+    oName = '/Users/danielhamill/git_clones/Time_Series/output/mc_variability.png'
 elif platform.system() == 'Windows':
-    dB_file = r'C:\workspace\sandbar_process\Merged_Sandbar_data.csv'
-    lt_sties = r'C:\workspace\sandbar_process\sites.xlsx'
-
-
+    db_file = r'C:\workspace\sandbar_process\Merged_Sandbar_data.csv'
+    lt_sites = r"C:\workspace\Time_Series\sites.xlsx"
+    oName = r'C:\workspace\Time_Series\output\mc_variability.png'
+    
 #Load Database    
 data = pd.read_csv(db_file,sep=',')    
 
@@ -52,6 +95,15 @@ long_term = data[data['Site'].isin(set(lu_sites['Sediment Deficit Sites']))]
 #Get data for plotting
 long_term = query_df(long_term)
 all_sites = query_df(data)
+all_sites = all_sites.query('Bar_type != "Total"')
+
+#Find extra sites
+all_site_df = pd.DataFrame(data=all_sites.Site.unique(),columns=['Site'])
+lt_site_df = pd.DataFrame(data=long_term.Site.unique(),columns=['Site'])
+extra_sites = all_site_df[~all_site_df.isin(set(lt_site_df['Site']))].dropna()
+
+#find influence of extra sites
+influence_df = change_finder(extra_sites,long_term,all_sites)
 
 #Data to plot                 
 long_term = pivot_df(long_term)
@@ -61,5 +113,9 @@ fig,ax = plt.subplots()
 long_term.plot(y = 'Norm_Vol', yerr='Norm_Error',ax = ax, label = 'Marble Canyon N=12',color='blue',marker='o')
 all_sites.plot(y = 'Norm_Vol', yerr='Norm_Error',ax = ax, label = 'Marble Canyon N=30',color='green',linestyle='--',marker='x')
 plt.tight_layout()
-plt.savefig('/Users/danielhamill/git_clones/Time_Series/output/mc_variability.png',dpi=600)
+#plt.savefig(oName,dpi=600)
 plt.show()
+
+
+
+
