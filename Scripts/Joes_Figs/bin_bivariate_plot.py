b5a287aa0f6b112b401c2940ef30894503326c12
danhamill <hamill.daniel@gmail.com>
2017-06-22 15:38:39 -0700

    added individual bar type plots and added hatching to denote analysis periods

--- Scripts/Joes_Figs/bin_bivariate_plot.py
+++ Scripts/Joes_Figs/bin_bivariate_plot.py
@@ -200,24 +200,236 @@ u.to_excel(writer,sheet_name='Undifferentiated')
 writer.save()
 del writer, s,u,r
 
+#################################################################################################################################
+##                                      Area Plots
+#################################################################################################################################
+
+#All data (Reatt and Other)
+
 #Linear regression
 s,i,r,p,stderr = linregress(r_fz_a['Norm_Area'], r_he_a['Norm_Area'])
 
 
-#Area Plot
 fig, ax = plt.subplots(figsize=(7.5,5))
-a = ax.scatter(x=r_fz_a['Norm_Area'], y=r_he_a['Norm_Area'], marker='x',c='k',label='Reattachment',s=30)
+#Deficit
+a = ax.scatter(x=r_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], y=r_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], 
+               marker='D',facecolor='w',label='Reattachment: Deficit',s=100,hatch='...')
+#Enrichment
+b = ax.scatter(x=r_fz_a.loc['2003-09-20':,:]['Norm_Area'], y=r_he_a.loc['2003-09-20':,:]['Norm_Area'], 
+               marker='D',facecolor='w',label='Reattachment: Enrichment ',s=100,hatch='|||||')
 ax.plot(r_fz_a['Norm_Area'], i +s*r_fz_a['Norm_Area'],'k')
-b = ax.scatter(x=s_fz_a['Norm_Area'], y=s_he_a['Norm_Area'], marker='o',c='grey',alpha=0.4,label='Other')
-ax.scatter(x=u_fz_a['Norm_Area'], y=u_he_a['Norm_Area'], marker='o',c='grey',alpha=0.4)
-legend = ax.legend(handles=[a,b],loc=9,fontsize='small',ncol=2,title='Bar Type')
+
+#Deficit
+c = ax.scatter(x=s_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], y=s_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], 
+               marker='o',facecolor='w',label='Other: Deficit',s=100,hatch='...',alpha=0.4)
+ax.scatter(x=u_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], y=u_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], 
+               marker='o',facecolor='w',s=100,hatch='...',alpha=0.4)
+#Enrichment
+d = ax.scatter(x=u_fz_a.loc['2003-09-20':,:]['Norm_Area'], y=u_he_a.loc['2003-09-20':,:]['Norm_Area'], 
+               marker='o',facecolor='w',label='Other: Enrichment',s=100,hatch='|||||',alpha=0.4)
+ax.scatter(x=s_fz_a.loc['2003-09-20':,:]['Norm_Area'], y=s_he_a.loc['2003-09-20':,:]['Norm_Area'], 
+               marker='o',facecolor='w',s=100,hatch='|||||',alpha=0.4)
+
+legend = ax.legend(handles=[a,b,c,d],loc=9,fontsize='small',ncol=2,title='Bar Type: Analysis Period',scatterpoints=1)
 ax.set_ylabel('High Elevation Normalized Area')
 ax.set_xlabel('Fluctuating Zone Normalized Area')
-ax.text(0.18,0.12,'R$^2$=%1.4f' % r )
+ax.text(0.16,0.3,'R$_{reatt}$$^2$=%1.4f' % r )
+ax.set_ylim(0.1,0.4)
 plt.setp(legend.get_title(),fontsize='small')
 plt.tight_layout()
-plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\Norm_Area_bin_bivariate.png",dpi=600)
+plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\All_Norm_Area_bin_bivariate.png")
 
+
+#######################################Undiff Data
+#Linear regression
+s,i,r,p,stderr = linregress(u_fz_a.loc['2003-09-20':,:]['Norm_Area'], u_he_a.loc['2003-09-20':,:]['Norm_Area'])
+s1,i1,r1,p1,stderr1 = linregress(u_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], u_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'])
+
+fig, ax = plt.subplots(figsize=(7.5,5))
+ax.scatter(x=u_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], y=u_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], 
+               marker='o',facecolor='w',s=100,hatch='...',alpha=0.4,label='Unidfferentiated: Deficit')
+ax.scatter(x=u_fz_a.loc['2003-09-20':,:]['Norm_Area'], y=u_he_a.loc['2003-09-20':,:]['Norm_Area'], 
+               marker='o',facecolor='w',label='Unidfferentiated: Enrichment',s=100,hatch='|||||',alpha=0.4)
+ax.plot(u_fz_a['Norm_Area'], i +s*u_fz_a['Norm_Area'],'k',label='Linear Regression: Enrichment')
+ax.plot(u_fz_a['Norm_Area'], i1 +s1*u_fz_a['Norm_Area'],'k',label='Linear Regression: Deficit',ls=':')
+legend = ax.legend(loc=9,fontsize='small',ncol=2,title='Bar Type: Analysis Period',scatterpoints=1)
+ax.set_ylabel('High Elevation Normalized Area')
+ax.set_xlabel('Fluctuating Zone Normalized Area')
+ax.text(0.16,0.3,'R$_{enrich}$$^2$=%1.4f' % r )
+ax.text(0.16,0.28,'m$_{enrich}$$^2$=%1.4f' % s )
+ax.text(0.34,0.3,'R$_{deficit}$$^2$=%1.4f' % r1 )
+ax.text(0.34,0.28,'m$_{deficit}$$^2$=%1.4f' % s1 )
+
+ax.set_ylim(0.1,0.4)
+plt.setp(legend.get_title(),fontsize='small')
+plt.tight_layout()
+plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\undiff_Norm_Area_bin_bivariate.png")
+
+#######################################Reatt Data
+#Linear regression
+s,i,r,p,stderr = linregress(r_fz_a.loc['2003-09-20':,:]['Norm_Area'], r_he_a.loc['2003-09-20':,:]['Norm_Area'])
+s1,i1,r1,p1,stderr1 = linregress(r_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], r_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'])
+
+fig, ax = plt.subplots(figsize=(7.5,5))
+ax.scatter(x=r_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], y=r_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], 
+               marker='o',facecolor='w',s=100,hatch='...',alpha=0.4,label='Reattachment: Deficit')
+ax.scatter(x=r_fz_a.loc['2003-09-20':,:]['Norm_Area'], y=r_he_a.loc['2003-09-20':,:]['Norm_Area'], 
+               marker='o',facecolor='w',label='Reattachment: Enrichment',s=100,hatch='|||||',alpha=0.4)
+ax.plot(r_fz_a['Norm_Area'], i +s*r_fz_a['Norm_Area'],'k',label='Linear Regression: Enrichment')
+ax.plot(r_fz_a['Norm_Area'], i1 +s1*r_fz_a['Norm_Area'],'k',label='Linear Regression: Deficit',ls=':')
+legend = ax.legend(loc=9,fontsize='small',ncol=2,title='Bar Type: Analysis Period',scatterpoints=1)
+ax.set_ylabel('High Elevation Normalized Area')
+ax.set_xlabel('Fluctuating Zone Normalized Area')
+ax.text(0.16,0.28,'R$_{enrich}$$^2$=%1.4f' % r )
+ax.text(0.16,0.26,'m$_{enrich}$$^2$=%1.4f' % s )
+ax.text(0.34,0.28,'R$_{deficit}$$^2$=%1.4f' % r1 )
+ax.text(0.34,0.26,'m$_{deficit}$$^2$=%1.4f' % s1 )
+ax.set_ylim(0.05,0.35)
+plt.setp(legend.get_title(),fontsize='small')
+plt.tight_layout()
+plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\reatt_Norm_Area_bin_bivariate.png")
+
+#######################################Sep Data
+#Linear regression
+s,i,r,p,stderr = linregress(s_fz_a.loc['2003-09-20':,:]['Norm_Area'], s_he_a.loc['2003-09-20':,:]['Norm_Area'])
+s1,i1,r1,p1,stderr1 = linregress(s_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], s_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'])
+
+fig, ax = plt.subplots(figsize=(7.5,5))
+ax.scatter(x=s_fz_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], y=s_he_a.loc['1990-06-10':'2003-09-20',:]['Norm_Area'], 
+               marker='o',facecolor='w',s=100,hatch='...',alpha=0.4,label='Separation: Deficit')
+ax.scatter(x=s_fz_a.loc['2003-09-20':,:]['Norm_Area'], y=s_he_a.loc['2003-09-20':,:]['Norm_Area'], 
+               marker='o',facecolor='w',label='Separation: Enrichment',s=100,hatch='|||||',alpha=0.4)
+ax.plot(s_fz_a['Norm_Area'], i +s*s_fz_a['Norm_Area'],'k',label='Linear Regression: Enrichment')
+ax.plot(s_fz_a['Norm_Area'], i1 +s1*s_fz_a['Norm_Area'],'k',label='Linear Regression: Deficit',ls=':')
+legend = ax.legend(loc=9,fontsize='small',ncol=2,title='Bar Type: Analysis Period',scatterpoints=1)
+ax.set_ylabel('High Elevation Normalized Area')
+ax.set_xlabel('Fluctuating Zone Normalized Area')
+ax.text(0.20,0.23,'R$_{enrich}$$^2$=%1.4f' % r )
+ax.text(0.20,0.22,'m$_{enrich}$$^2$=%1.4f' % s )
+ax.text(0.28,0.23,'R$_{deficit}$$^2$=%1.4f' % r1 )
+ax.text(0.28,0.22,'m$_{deficit}$$^2$=%1.4f' % s1 )
+ax.set_ylim(0.12,0.27)
+plt.setp(legend.get_title(),fontsize='small')
+plt.tight_layout()
+plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\sep_Norm_Area_bin_bivariate.png")
+
+
+
+#################################################################################################################################
+##                                      Volume Plots
+#################################################################################################################################
+
+#All data (Reatt and Other)
+
+#Linear regression
+s,i,r,p,stderr = linregress(r_fz_v['Norm_Vol'], r_he_v['Norm_Vol'])
+
+
+fig, ax = plt.subplots(figsize=(7.5,5))
+#Deficit
+a = ax.scatter(x=r_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], y=r_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], 
+               marker='D',facecolor='w',label='Reattachment: Deficit',s=100,hatch='...')
+#Enrichment
+b = ax.scatter(x=r_fz_v.loc['2003-09-20':,:]['Norm_Vol'], y=r_he_v.loc['2003-09-20':,:]['Norm_Vol'], 
+               marker='D',facecolor='w',label='Reattachment: Enrichment ',s=100,hatch='|||||')
+ax.plot(r_fz_v['Norm_Vol'], i +s*r_fz_v['Norm_Vol'],'k')
+
+#Deficit
+c = ax.scatter(x=s_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], y=s_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], 
+               marker='o',facecolor='w',label='Other: Deficit',s=100,hatch='...',alpha=0.4)
+ax.scatter(x=u_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], y=u_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], 
+               marker='o',facecolor='w',s=100,hatch='...',alpha=0.4)
+#Enrichment
+d = ax.scatter(x=u_fz_v.loc['2003-09-20':,:]['Norm_Vol'], y=u_he_v.loc['2003-09-20':,:]['Norm_Vol'], 
+               marker='o',facecolor='w',label='Other: Enrichment',s=100,hatch='|||||',alpha=0.4)
+ax.scatter(x=s_fz_v.loc['2003-09-20':,:]['Norm_Vol'], y=s_he_v.loc['2003-09-20':,:]['Norm_Vol'], 
+               marker='o',facecolor='w',s=100,hatch='|||||',alpha=0.4)
+
+legend = ax.legend(handles=[a,b,c,d],loc=9,fontsize='small',ncol=2,title='Bar Type: Analysis Period',scatterpoints=1)
+ax.set_ylabel('High Elevation Normalized Volume')
+ax.set_xlabel('Fluctuating Zone Normalized Volume')
+ax.text(0.25,0.6,'R$_{Reatt}$$^2$=%1.4f' % r )
+#ax.set_ylim(0.1,0.4)
+plt.setp(legend.get_title(),fontsize='small')
+plt.tight_layout()
+plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\All_Norm_Vol_bin_bivariate.png")
+
+
+#######################################Undiff Data
+#Linear regression
+s,i,r,p,stderr = linregress(u_fz_v.loc['2003-09-20':,:]['Norm_Vol'], u_he_v.loc['2003-09-20':,:]['Norm_Vol'])
+s1,i1,r1,p1,stderr1 = linregress(u_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], u_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'])
+
+fig, ax = plt.subplots(figsize=(7.5,5))
+ax.scatter(x=u_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], y=u_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], 
+               marker='o',facecolor='w',s=100,hatch='...',alpha=0.4,label='Unidfferentiated: Deficit')
+ax.scatter(x=u_fz_v.loc['2003-09-20':,:]['Norm_Vol'], y=u_he_v.loc['2003-09-20':,:]['Norm_Vol'], 
+               marker='o',facecolor='w',label='Unidfferentiated: Enrichment',s=100,hatch='|||||',alpha=0.4)
+ax.plot(u_fz_v['Norm_Vol'], i +s*u_fz_v['Norm_Vol'],'k',label='Linear Regression: Enrichment')
+ax.plot(u_fz_v['Norm_Vol'], i1 +s1*u_fz_v['Norm_Vol'],'k',label='Linear Regression: Deficit',ls=':')
+legend = ax.legend(loc=9,fontsize='small',ncol=2,title='Bar Type: Analysis Period',scatterpoints=1)
+ax.set_ylabel('High Elevation Normalized Volume')
+ax.set_xlabel('Fluctuating Zone Normalized Volume')
+ax.text(0.28,0.52,'R$_{enrich}$$^2$=%1.4f' % r )
+ax.text(0.28,0.49,'m$_{enrich}$$^2$=%1.4f' % s )
+ax.text(0.55,0.27,'R$_{deficit}$$^2$=%1.4f' % r1 )
+ax.text(0.55,0.24,'m$_{deficit}$$^2$=%1.4f' % s1 )
+#
+#ax.set_ylim(0.1,0.4)
+plt.setp(legend.get_title(),fontsize='small')
+plt.tight_layout()
+plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\undiff_Norm_Vol_bin_bivariate.png")
+
+#######################################Reatt Data
+#Linear regression
+s,i,r,p,stderr = linregress(r_fz_v.loc['2003-09-20':,:]['Norm_Vol'], r_he_v.loc['2003-09-20':,:]['Norm_Vol'])
+s1,i1,r1,p1,stderr1 = linregress(r_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], r_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'])
+
+fig, ax = plt.subplots(figsize=(7.5,5))
+ax.scatter(x=r_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], y=r_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], 
+               marker='o',facecolor='w',s=100,hatch='...',alpha=0.4,label='Reattachment: Deficit')
+ax.scatter(x=r_fz_v.loc['2003-09-20':,:]['Norm_Vol'], y=r_he_v.loc['2003-09-20':,:]['Norm_Vol'], 
+               marker='o',facecolor='w',label='Reattachment: Enrichment',s=100,hatch='|||||',alpha=0.4)
+ax.plot(r_fz_v['Norm_Vol'], i +s*r_fz_v['Norm_Vol'],'k',label='Linear Regression: Enrichment')
+ax.plot(r_fz_v['Norm_Vol'], i1 +s1*r_fz_v['Norm_Vol'],'k',label='Linear Regression: Deficit',ls=':')
+legend = ax.legend(loc=9,fontsize='small',ncol=2,title='Bar Type: Analysis Period',scatterpoints=1)
+ax.set_ylabel('High Elevation Normalized Volume')
+ax.set_xlabel('Fluctuating Zone Normalized Volume')
+ax.text(0.28,0.52,'R$_{enrich}$$^2$=%1.4f' % r )
+ax.text(0.28,0.49,'m$_{enrich}$$^2$=%1.4f' % s )
+ax.text(0.51,0.33,'R$_{deficit}$$^2$=%1.4f' % r1 )
+ax.text(0.51,0.30,'m$_{deficit}$$^2$=%1.4f' % s1 )
+#ax.set_ylim(0.05,0.35)
+plt.setp(legend.get_title(),fontsize='small')
+plt.tight_layout()
+plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\reatt_Norm_Vol_bin_bivariate.png")
+
+#######################################Sep Data
+#Linear regression
+s,i,r,p,stderr = linregress(s_fz_v.loc['2003-09-20':,:]['Norm_Vol'], s_he_v.loc['2003-09-20':,:]['Norm_Vol'])
+s1,i1,r1,p1,stderr1 = linregress(s_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], s_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'])
+
+fig, ax = plt.subplots(figsize=(7.5,5))
+ax.scatter(x=s_fz_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], y=s_he_v.loc['1990-06-10':'2003-09-20',:]['Norm_Vol'], 
+               marker='o',facecolor='w',s=100,hatch='...',alpha=0.4,label='Separation: Deficit')
+ax.scatter(x=s_fz_v.loc['2003-09-20':,:]['Norm_Vol'], y=s_he_v.loc['2003-09-20':,:]['Norm_Vol'], 
+               marker='o',facecolor='w',label='Separation: Enrichment',s=100,hatch='|||||',alpha=0.4)
+ax.plot(s_fz_v['Norm_Vol'], i +s*s_fz_v['Norm_Vol'],'k',label='Linear Regression: Enrichment')
+ax.plot(s_fz_v['Norm_Vol'], i1 +s1*s_fz_v['Norm_Vol'],'k',label='Linear Regression: Deficit',ls=':')
+legend = ax.legend(loc=9,fontsize='small',ncol=2,title='Bar Type: Analysis Period',scatterpoints=1)
+ax.set_ylabel('High Elevation Normalized Volume')
+ax.set_xlabel('Fluctuating Zone Normalized Volume')
+ax.text(0.16,0.45,'R$_{enrich}$$^2$=%1.4f' % r )
+ax.text(0.16,0.43,'m$_{enrich}$$^2$=%1.4f' % s )
+ax.text(0.56,0.32,'R$_{deficit}$$^2$=%1.4f' % r1 )
+ax.text(0.56,0.30,'m$_{deficit}$$^2$=%1.4f' % s1 )
+ax.set_ylim(0.2,0.60)
+plt.setp(legend.get_title(),fontsize='small')
+plt.tight_layout()
+plt.savefig(r"c:\workspace\Time_Series\Output\Joes_Figs\sep_Norm_Volume_bin_bivariate.png")
+
+
+####################################################################################################################################
 
 #Linear regression
 s,i,r,p,stderr = linregress(r_fz_v['Norm_Vol'], r_he_v['Norm_Vol'])
